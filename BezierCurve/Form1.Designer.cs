23c42d16fdf82c008bfd15c74336a5540be781c3
Musa Berk İşkal <124535185+mberkiskal@users.noreply.github.com>
2023-02-28 09:06:41 +0300

    Bilgisayar Grafikleri ile Bezier Egrisi (c#)
    Bilgisayar Grafikleri Proje(C#): Analitik düzlemde koordinat sistemi oluşturma, Kontrol noktalarını belirtme ve Bezier Eğrisi ile kontrol noktalarını birleştirme.

---
+++ BezierCurve/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Musa_Berk_ISKAL_1200505026
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Koordinat Eksenlerini Çiz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.koordinat_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 58);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Kontrol Noktalarını Çiz";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.noktalar_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 58);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Bezier Eğrisini Çiz";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.bezier_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BezierCurve/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+
+
+namespace Musa_Berk_ISKAL_1200505026
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void koordinat_Click(object sender, MouseEventArgs e)
+        {
+            System.Drawing.Graphics grafiknesne;
+            grafiknesne = this.CreateGraphics();
+            Pen firca = new Pen(System.Drawing.Color.Green, 3);
+            //X Düzlemi
+            grafiknesne.DrawLine(firca, 0, 250, 800, 250);
+            //Y Düzlemi
+            grafiknesne.DrawLine(firca, 400, 0, 400, 500);
+
+            Brush firca1= new SolidBrush(System.Drawing.Color.Red);
+            Font yazis = new System.Drawing.Font("Helvetica", 8);
+            //System.Drawing.Graphics elips;
+            //elips = this.CreateGraphics();
+        }
+
+        private void bezier_Click(object sender, MouseEventArgs e)
+        {
+            //BezierEğrisi3
+            System.Drawing.Graphics Graphics;
+            Graphics = this.CreateGraphics();
+            PointF p0 = new PointF(400, 250);
+            PointF p1 = new PointF(475, 55);
+            PointF p2 = new PointF(675, 185);
+            PointF p3 = new PointF(640, 250);
+
+            Pen bezier = new Pen(System.Drawing.Color.Black, 2);
+          
+
+            //BezierEğrisi2
+            Point[] p = {
+            new Point(10, 100),   
+            new Point(75, 10),    
+            new Point(80, 50),    
+            new Point(100, 150),  
+            new Point(125, 80),   
+            new Point(175, 200), 
+            new Point(200, 80)}; 
+
+            Pen pen = new Pen(Color.Blue);
+            Graphics.DrawBezier(bezier, p0, p1, p2, p3);
+            //
+
+
+        }
+
+        private void noktalar_Click(object sender, EventArgs e)
+        {
+            Pen firca = new Pen(System.Drawing.Color.Red, 3);
+            System.Drawing.Graphics elips;
+            elips = this.CreateGraphics();
+
+            //Elips
+            //Kontrol Noktası0
+            Pen p0 = new Pen(System.Drawing.Color.Red, 3);
+            elips.DrawEllipse(firca, 390, 240, 20, 20);
+            //Kontrol Noktası1
+            Pen p1 = new Pen(System.Drawing.Color.Red, 3);
+            elips.DrawEllipse(firca, 470, 60, 20, 20);
+            //Kontrol Noktası2
+            Pen p2 = new Pen(System.Drawing.Color.Red, 3);
+            elips.DrawEllipse(firca, 670, 180, 20, 20);
+            //Kontrol Noktası3
+            Pen p3 = new Pen(System.Drawing.Color.Red, 3);
+            elips.DrawEllipse(firca, 630, 240, 20, 20);
+
+
+            System.Drawing.Graphics graphicsObj;
+            graphicsObj = this.CreateGraphics();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //BezierEğrisi3
+            Point p1 = new Point(10, 100);   
+            Point c1 = new Point(100, 10);   
+            Point c2 = new Point(150, 150);  
+            Point p2 = new Point(200, 100);  
+
+            Pen pen = new Pen(Color.FromArgb(255, 0, 0, 255));
+            
+        }
+
+       
+    }
+}
